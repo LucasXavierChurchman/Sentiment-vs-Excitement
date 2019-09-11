1a22e3f7a52b3e0c43327e07b6e2898b5572f4e9
LucasXavierChurchman <lucasxavierchurchman@gmail.com>
2019-09-11 10:33:51 -0400

    main plot made, still needs to be prettied up

--- src/EDA.py
+++ src/EDA.py
@@ -42,38 +42,67 @@ names = ['r/NBA Comments',
         'r/NBA Comments with Warriors Flair',
         'r/ClevelandCavs comments'
         'r/Warriors comments']
+top=0.975,
+bottom=0.025,
+left=0.025,
+right=0.975,
+hspace=0.2,
+wspace=0.04
 
-
-####### Plot mean sentiment scores ######
-plt.style.use('seaborn-darkgrid')
+####### Plot mean and frequency sentiment scores ######
+plt.style.use('ggplot')
 matplotlib.rc('lines', linewidth=3)
-fig, ((ax1, ax2, ax3),(ax4,ax5,ax6)) = plt.subplots(2,3,figsize=(20, 8), gridspec_kw={'hspace': 0})
+fig, ((ax1, ax2, ax3),(ax4, ax5, ax6)) = plt.subplots(2,3,figsize=(30, 8))
 
+#rNba
 nba_data = nba_df[['time_slice','sentiment_score']].groupby('time_slice').agg([np.mean, np.sum, np.size])
 x = np.linspace(0, nba_data.shape[0], num = nba_data.shape[0])
 ax1.plot(x, nba_data['sentiment_score']['mean'], color = 'orangered', alpha = 0.5)
 ax1.set_ylim(-2.99,2.99)
-ax1.set_title('All Comments (/r/nba)')
+ax1.set_title('Mean Sentiment Score /r/NBA')
+ax1.legend(('r/NBA',), loc = 'upper left')
+
+ax4.plot(x, nba_data['sentiment_score']['size'], color = 'orangered', alpha = 0.5)
+# ax4.set_title('Comment Count /r/NBA')
+ax4.legend(('r/NBA',), loc = 'upper left')
 plt.grid()
 
+#Cavs
 cavs_data = cavs_df[['time_slice','sentiment_score']].groupby('time_slice').agg([np.mean, np.sum, np.size])
 flair_cavs_data = flair_cavs_df[['time_slice','sentiment_score']].groupby('time_slice').agg([np.mean, np.sum, np.size])
 ax2.plot(x, cavs_data['sentiment_score']['mean'], color = 'maroon', alpha = 0.5)
 ax2.plot(x, flair_cavs_data['sentiment_score']['mean'], color = 'red', alpha = 0.5)
 ax2.set_ylim(-2.99,2.99)
-ax2.set_title('Cavaliers Fan Comments')
+ax2.set_title('Mean Sentiment Score Cavaliers Fan Comments')
+ax2.legend(('Cavs Subreddit','rNBA Cavs Flair'), loc = 'upper left')
 
+ax5.plot(x, cavs_data['sentiment_score']['size'], color = 'maroon', alpha = 0.5)
+ax5.plot(x, flair_cavs_data['sentiment_score']['size'], color = 'red', alpha = 0.5)
+# ax5.set_title('Comment Count Cavs')
+ax5.legend(('Cavs Subreddit','rNBA Cavs Flair'), loc = 'upper left')
+
+#Warriors
 dubs_data = dubs_df[['time_slice','sentiment_score']].groupby('time_slice').agg([np.mean, np.sum, np.size])
 flair_dubs_data = flair_dubs_df[['time_slice','sentiment_score']].groupby('time_slice').agg([np.mean, np.sum, np.size])
 ax3.plot(x, dubs_data['sentiment_score']['mean'], color = 'darkblue', alpha = 0.5)
-ax3.plot(x, flair_dubs_data['sentiment_score']['mean'], color = 'blue', alpha = 0.5)
+ax3.plot(x, flair_dubs_data['sentiment_score']['mean'], color = 'turquoise', alpha = 0.5)
 ax3.set_ylim(-2.99,2.99)
-ax3.set_title('Warriors Fan Comments')
+ax3.set_title('Mean Sentiment Score Warriors Fan Comments')
 
+ax6.plot(x, dubs_data['sentiment_score']['size'], color = 'darkblue', alpha = 0.5)
+ax6.plot(x, flair_dubs_data['sentiment_score']['size'], color = 'turquoise', alpha = 0.5)
+# ax6.set_title('Comment Count Warriors')
+ax6.legend(('Warriors Subreddit','rNBA Warriors Flair'), loc = 'upper left')
 
-for ax in fig.get_axes():
-    ax.label_outer()
+plt.grid()
+plt.subplots_adjust(top=0.975,
+                    bottom=0.025,
+                    left=0.025,
+                    right=0.975,
+                    hspace=0.2,
+                    wspace=0.04)
+
+# for ax in fig.get_axes():
+#     ax.label_outer()
 
 plt.show()
-
-# ax2.hist(data['sentiment_score']['mean'], color = 'orangered', bins = 50, alpha = 0.5)
