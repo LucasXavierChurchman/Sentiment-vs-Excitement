ae32c291ea8c688e54bcdcc8dad681a76a108b6f
LucasXavierChurchman <lucasxavierchurchman@gmail.com>
2019-09-13 10:55:59 -0400

    changed overlap plot scaling

--- src/SentimentScoreCommentDensityOverlap.py
+++ src/SentimentScoreCommentDensityOverlap.py
@@ -28,7 +28,7 @@ flair_dubs_data = flair_dubs_df[['time_slice','sentiment_score']].groupby('time_
 
 def plot_sentiment():
     matplotlib.rc('lines', linewidth=3)
-    fig, ax = plt.subplots(figsize=(15, 4))
+    fig, ax = plt.subplots(figsize=(18, 6))
 
     x = np.linspace(0, nba_data.shape[0], num = nba_data.shape[0])
     ax.plot(x, nba_data['sentiment_score']['mean'], color = 'orangered', alpha = 0.5)
@@ -53,7 +53,7 @@ def plot_sentiment():
 
 def plot_density():
     matplotlib.rc('lines', linewidth=3)
-    fig, ax = plt.subplots(figsize=(20, 4))
+    fig, ax = plt.subplots(figsize=(18, 6))
 
     x = np.linspace(0, nba_data.shape[0], num = nba_data.shape[0])
     # ax.plot(x, nba_data['sentiment_score']['size'], color = 'orangered', alpha = 0.5) # skews the plot
